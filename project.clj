f7ea4a1c2a6f21c4b0eeca3d3c856a20be19ba24
Shaobo Hou <shaobohou@gmail.com>
2013-08-15 12:31:49 +0100

    getting tracks info

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject coverme "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :min-lein-version "2.0.0"
+
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.7.6"]
+                 [cheshire "5.2.0"]]
+
+  :main coverme.core)
--- src/coverme/core.clj
+++ src/coverme/core.clj
@@ -1,6 +1,42 @@
-(ns coverme.core)
+(ns coverme.core
+  (:require [clj-http.client :as client]
+            [cheshire.core :as json]
+            [clojure.string :as string])
+  (:use [clojure.pprint])
+  (:gen-class))
 
-(defn foo
+(defn str-to-web
+  [s]
+  (string/join "%20" (string/split s #" ")))
+
+(defn trim-tracks
+  [tracks]
+  (let [trim-fn (fn [track]
+                  (let [artist (-> track (get "artists") first (get "name"))]
+                    (merge (select-keys track ["name" "popularity"])
+                           {"artist" artist})))]    
+    (map trim-fn tracks)))
+
+(defn get-tracks
+  [q]
+  (let [query (str "http://ws.spotify.com/search/1/track.json?q=" q)]
+    (trim-tracks (second (second (json/parse-string (:body (client/get query))))))))
+
+(defn get-tracks-by-artist
+  "unique by track name"
+  [artist]
+  (get-tracks (str "artist%3a" (str-to-web artist))))
+
+(defn get-tracks-by-title
+  "unique by artist name"
+  [title]
+  (get-tracks (str (str-to-web title))))
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [& x]
+  (let [tracks (get-tracks-by-artist "Nina Simone")
+        tracks (get-tracks-by-title  "I put a a spell on you")]
+    (pprint (take 3 tracks)))
+  (pprint (str-to-web "Nina Simone"))
+  )
