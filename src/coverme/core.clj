6220b797524a5b1a3fd24d9c839fa41aaf5e2d12
Shaobo Hou <shaobohou@gmail.com>
2013-08-15 15:30:26 +0100

    basic code working, need to robustify

--- src/coverme/core.clj
+++ src/coverme/core.clj
@@ -5,6 +5,10 @@
   (:use [clojure.pprint])
   (:gen-class))
 
+(defn trim-title
+  [title]
+  (string/trim (first (string/split title #"-"))))
+
 (defn str-to-web
   [s]
   (string/join "%20" (string/split s #" ")))
@@ -14,11 +18,11 @@
   ;; (println)
   ;; (pprint (take 3 tracks))
   (let [trim-fn (fn [track]
-                  (let [artist (-> track (get "artists") first)
+                  (let [artists (-> track (get "artists") first)
                         href (get track "href")]
                     (merge (select-keys track ["name" "popularity"])
-                           {"artists" (get artist "name")
-                            ;; "artists-href" (get artist "href")
+                           {"artists" (get artists "name")
+                            ;; "artists-href" (get artists "href")
                             ;; "name-href" (get track "href")
                             })))]
     (map trim-fn tracks)))
@@ -39,31 +43,56 @@
   (let [query (str "http://ws.spotify.com/search/1/track.json?q=" q)]
     (trim-tracks (second (second (json/parse-string (:body (client/get query))))))))
 
-(defn get-tracks-by-artist
+(defn get-tracks-by-artists
   "unique by track name"
-  [artist]
-  (-> (str "artist%3a" (str-to-web artist))
+  [artists]
+  (-> (str "artist%3a" (str-to-web artists))
       get-tracks
       (unique-tracks "name")
-      ((fn [tracks] (map #(assoc % "artists" artist) tracks)))
+      ((fn [tracks] (map #(assoc % "artists" artists) tracks)))
       sort-tracks))
 
 (defn get-tracks-by-title
-  "unique by artist name"
-  [title]
-  (-> (str "track%3a" (str-to-web title))
-      get-tracks
-      (unique-tracks "artists")
-      ((fn [tracks] (map #(assoc % "name" title) tracks)))
-      sort-tracks))
+  "unique by artists name"
+  [full-title]
+  (let [title (trim-title full-title)]
+    (-> (str "track%3a" (str-to-web title))
+        get-tracks
+        (unique-tracks "artists")
+        ((fn [tracks] (map #(assoc % "name" title) tracks)))
+        sort-tracks)))
+
+(defn generate-playlist-from-artists
+  "Given an artist and a song. Find a more popular/different song by the same artist. Find a different artist that covers the song. Repeat the with new artist and song"
+  [artists title]
+  (Thread/sleep 1000)
+  (let [artists-tracks (take 5 (filter #(not= (get % "name") title) (get-tracks-by-artists artists)))
+        rand-song      (first (shuffle artists-tracks))
+        new-title      (trim-title (get rand-song "name"))
+        title-tracks   (take 5 (filter #(not= (get % "artists") artists) (get-tracks-by-title new-title)))
+        rand-song2     (first (shuffle title-tracks))
+        new-artists    (get rand-song2 "artists")
+        ]
+    (println)
+    (println new-title " BY " artists " WAS ALSO COVERED BY " new-artists)
+    (pprint rand-song2)
+    (println)
+    (cons rand-song
+          (lazy-seq (generate-playlist-from-artists new-artists new-title)))))
 
 (defn -main
   "I don't do a whole lot."
   [& x]
-  (let [tracks-by-artist (get-tracks-by-artist "Nina Simone")
-        tracks-by-name   (get-tracks-by-title  "I Put A Spell On You")]
+  (let [tracks-by-artists (get-tracks-by-artists "Nina Simone")
+        tracks-by-name    (get-tracks-by-title   "I Put A Spell On You")]
     (println)
-    (pprint (take 5 tracks-by-artist))
+    (pprint (take 5 tracks-by-artists))
     (println)
     (pprint (take 5 tracks-by-name)))
+
+  (println "\n\n\n")
+  
+  (println "\n\n\n")
+  (print (count (take 10 (generate-playlist-from-artists "Nina Simone" ""))))
+  (println "\n\n\nFinished!!!!")
   )
